ea9be9165cd74de12d0107eda50b7c6d09704b14
LeeKyooBaek <charisma1124@gmail.com>
2021-08-20 23:14:56 +0900

    lorem

--- syntax/string.js
+++ syntax/string.js
@@ -1,2 +1,7 @@
-"1"
-'1'
+//javascript string count 
+
+var str = "HEllo World!";
+str.length;
+
+console.log('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa nesciunt dicta totam dolor aliquid tenetur vel, fugiat quo ducimus ipsum magni commodi, voluptatibus maxime doloremque magnam eligendi consectetur at quod.')
+
