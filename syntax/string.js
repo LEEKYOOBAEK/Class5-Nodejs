6a6ee315275fadd99f3fbb5d0f39b58e81de31cb
LeeKyooBaek <charisma1124@gmail.com>
2021-08-20 23:16:56 +0900

    str.length

--- syntax/string.js
+++ syntax/string.js
@@ -3,5 +3,5 @@
 var str = "HEllo World!";
 str.length;
 
-console.log('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa nesciunt dicta totam dolor aliquid tenetur vel, fugiat quo ducimus ipsum magni commodi, voluptatibus maxime doloremque magnam eligendi consectetur at quod.')
-
+console.log('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa nesciunt dicta totam dolor aliquid tenetur vel, fugiat quo ducimus ipsum magni commodi, voluptatibus maxime doloremque magnam eligendi consectetur at quod.');
+console.log(str.length);
