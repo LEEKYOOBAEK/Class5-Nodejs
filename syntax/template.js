e42491cd2091f13f57b989d68ec42c884218e2bd
LeeKyooBaek <charisma1124@gmail.com>
2021-08-21 02:48:43 +0900

    template literal

---
+++ syntax/template.js
@@ -0,0 +1,12 @@
+var name = 'k8805';
+var letter = 'Dear '+name+'\n\nLorem ipsum dolor sit amet, consectetur adipisicing elit. Est exercitationem, velit voluptatem architecto cupiditate fugiat repudiandae nulla accusamus dolor quaerat.Quaerat dolorem reprehenderit ea repellat, saepe, adipisci consequuntur non perspiciatis ad alias vero necessitatibus beatae ipsum quidem.Dolorum delectus consequatur amet porro!Nihil, esse deserunt asperiores possimus aperiam veritatis tempora consequuntur vitae ratione, quas molestias dicta illo.Provident unde corporis beatae alias explicabo voluptas, ratione asperiores odio facilis aliquam neque consectetur necessitatibus nam expedita ipsum.Architecto iure dignissimos molestias alias vitae laboriosam repellat dicta error magnam ? Aspernatur molestiae laudantium a temporibus culpa autem, sequi, amet voluptate, quis atque adipisci voluptatum ?'+name;
+
+console.log(letter);
+
+//node syntax/template.js
+
+var letter = `Dear ${name}
+
+asdasdasdasd +${name}`;
+
+console.log(letter);
---
+++ syntax/variable2.js
@@ -0,0 +1,5 @@
+var letter = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est exercitationem, velit voluptatem architecto cupiditate fugiat repudiandae nulla accusamus dolor quaerat. Quaerat dolorem reprehenderit ea repellat, saepe, adipisci consequuntur non perspiciatis ad alias vero necessitatibus beatae ipsum quidem. Dolorum delectus consequatur amet porro! Nihil, esse deserunt asperiores possimus aperiam veritatis tempora consequuntur vitae ratione, quas molestias dicta illo. Provident unde corporis beatae alias explicabo voluptas, ratione asperiores odio facilis aliquam neque consectetur necessitatibus nam expedita ipsum. Architecto iure dignissimos molestias alias vitae laboriosam repellat dicta error magnam? Aspernatur molestiae laudantium a temporibus culpa autem, sequi, amet voluptate, quis atque adipisci voluptatum?'
+
+console.log(letter);
+
+//node syntax/variable2.js
