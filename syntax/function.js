876533bb5d45b95f83e86c7bcfe907d51bd35676
LeeKyooBaek <charisma1124@gmail.com>
2021-08-24 00:27:03 +0900

    function

---
+++ syntax/function.js
@@ -0,0 +1,23 @@
+f123();
+console.log('A');
+console.log('Z');
+console.log('B');
+f123();
+console.log('F');
+console.log('C');
+console.log('P');
+console.log('J');
+f123();
+console.log('U');
+console.log('A');
+console.log('Z');
+console.log('J');
+console.log('I');
+//node syntax/function.js
+var i = 0;
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
