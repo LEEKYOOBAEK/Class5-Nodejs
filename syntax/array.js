eb5174829d154e25f5cf34fefda4c3e83c5d96f7
LeeKyooBaek <charisma1124@gmail.com>
2021-08-24 00:02:24 +0900

    array + while

--- syntax/array.js
+++ syntax/array.js
@@ -1,5 +1,9 @@
-var arr = ['A','B','C','D'];
-console.log(arr[1]);
-console.log(arr[3]);
-arr[2] = 3;
-console.log(arr);
+var number = [1,400,12,34];
+var i = 0;
+var total = 0;
+while(i<number.length){
+    total = total + number[i];
+    i = i+1;
+}
+console.log(`total : ${total}`);
+
