904ca89e0058e8284f788492b4b34749b6cbfafb
LeeKyooBaek <charisma1124@gmail.com>
2021-08-29 22:26:36 +0900

    nodejs file delete

--- main.js
+++ main.js
@@ -55,7 +55,12 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              ` <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>`
             );
             response.writeHead(200);
             response.end(template);
@@ -118,7 +123,7 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
-    } else if(pathname === '/update_process') {
+    } else if(pathname === '/update_process'){
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -128,14 +133,25 @@ var app = http.createServer(function(request,response){
           var id = post.id;
           var title = post.title;
           var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error) {
-            
+          fs.rename(`data/${id}`, `data/${title}`, function(error){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+            })
+          });
+      });
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(error){
+            response.writeHead(302, {Location: `/`});
+            response.end();
           })
-          console.log(post);
-          // fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          //   response.writeHead(302, {Location: `/?id=${title}`});
-          //   response.end();
-          // })
       });
     } else {
       response.writeHead(404);
