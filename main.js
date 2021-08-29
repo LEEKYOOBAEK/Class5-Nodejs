a8644e5bc9133b1f0d53ed761127e61509e72ccd
LeeKyooBaek <charisma1124@gmail.com>
2021-08-29 22:09:22 +0900

    잘안된다

--- main.js
+++ main.js
@@ -118,6 +118,25 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
+    } else if(pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(error) {
+            
+          })
+          console.log(post);
+          // fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          //   response.writeHead(302, {Location: `/?id=${title}`});
+          //   response.end();
+          // })
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
